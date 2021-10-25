558787ede9a2e5787e55c7906d7ab6ae84359803
Reyner-Arias <44006575+Reyner-Arias@users.noreply.github.com>
2021-10-25 12:41:35 -0600

    Fix node modules install command

--- src/Vista/IniciarSesion.js
+++ src/Vista/IniciarSesion.js
@@ -27,7 +27,7 @@ export default class IniciarSesion extends Component{
                 {console.log(this.state) }
                 <h2>
                     <div id="fondo">
-                    <h1>Login</h1>
+                    <h4>Por favor, ingrese los siguientes datos:</h4>
                     </div>
                 </h2>
                 <div className="center container w-25 p-8 py-2 my-3  mt-5">
@@ -36,8 +36,10 @@ export default class IniciarSesion extends Component{
                         <div>
                             <Form.Group onChange= {this.handleChange}>
                                 <br/>
+                                <h5>Correo:</h5>
                                 <Form.Control type="text" placeholder="User name" name = 'username' />
                                 <br/>
+                                <h5>Contraseña:</h5>
                                 <Form.Control type="password" placeholder="Password" name = 'password' />
                                 <br/>
                             </Form.Group>
@@ -45,11 +47,7 @@ export default class IniciarSesion extends Component{
                         </div>
                         <div className="d-grid gap-2 ">
                             <Button size="lg" variant="outline-success" type="submit">
-                                Login
-                            </Button>
-
-                            <Button variant="light" href='/sign' >
-                                    Register
+                                Iniciar Sesión
                             </Button>
                         </div>
                     </Form.Group>
