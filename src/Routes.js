18311ccf846883af3ccb82078db8a5bbad479833
Davenb42 <david.benavides.n@gmail.com>
2021-10-25 18:44:53 -0600

    Create Registrarse.js

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import IniciarSesion from './Vista/IniciarSesion';
+import Registrarse from './Vista/Registrarse'
 
 function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component ={IniciarSesion}/>
+                <Route exact path="/Registrarse" component ={Registrarse}/>
             </Switch>
         </BrowserRouter>
     );
---
+++ src/Vista/Registrarse.js
@@ -0,0 +1,93 @@
+import React, {Component} from 'react';
+import {Button, Nav, Navbar, Container, Form } from "react-bootstrap";
+import Cookies from "universal-cookie";
+import swal from "sweetalert";
+import NavStyle from "./css/NavStyle.css";
+
+const cookies = new Cookies();
+
+export default class IniciarSesion extends Component{
+
+    state = {
+        correo: '',
+        contrasena: '',
+        cedula: '',
+        nombre: '',
+        primerApellido: '',
+        segundoApellido: '',
+        telefono: '',
+    }
+
+    handleChange = e => {
+        this.setState({
+            ...this.state,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    enviar  = async (e) => {}
+
+    render(){
+        return(
+            <div>
+                <Navbar id="#navBar" collapseOnSelect bg="secondary" variant="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand id="loginTitle" href="#loginTitle">Registro</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link className="botonNav" href="#shoppingcart">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+                                        <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                    </svg>
+                                </Nav.Link>
+                                <Nav.Link className="botonNav" href="#galery">Galería</Nav.Link>
+                                <Nav.Link className="botonNav" href="#agenda">Agenda</Nav.Link>
+                            </Nav>
+                            <Nav>
+                                <Nav.Link className="botonNav" href="#sign-in">Iniciar Sesión</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+                <div className="center container w-50 p-8 py-2 my-3  mt-5">
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <div>
+                            <Form.Group onChange= {this.handleChange}>
+                                <h6>Por favor, ingrese los siguientes datos: </h6>
+                                <br/>
+                                <h6>Correo:</h6>
+                                <Form.Control type="text" name = 'correo' />
+                                <br/>
+                                <h6>Contraseña:</h6>
+                                <Form.Control type="password" name = 'contrasena' />
+                                <br/>
+                                <h6>Cédula:</h6>
+                                <Form.Control type="text" name = 'cedula' />
+                                <br/>
+                                <h6>Nombre:</h6>
+                                <Form.Control type="text" name = 'nombre' />
+                                <br/>
+                                <h6>Primer Apellido:</h6>
+                                <Form.Control type="text" name = 'primerApellido' />
+                                <br/>
+                                <h6>Segundo Apellido:</h6>
+                                <Form.Control type="text" name = 'segundoApellido' />
+                                <br/>
+                                <h6>Teléfono:</h6>
+                                <Form.Control type="text" name = 'telefono' />
+                                <br/>
+                            </Form.Group>
+
+                        </div>
+                        <div className="d-grid gap-2">
+                            <Button size="md" variant="secondary" type="submit">
+                                Iniciar Sesión
+                            </Button>
+                        </div>
+                    </Form.Group>
+                </div >
+            </div>
+        )
+    }
+}
