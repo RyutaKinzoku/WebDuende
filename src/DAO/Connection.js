e61dfb5ce206c1347e3b21be9600df14b64bc7cf
Reyner-Arias <44006575+Reyner-Arias@users.noreply.github.com>
2021-10-26 18:55:24 -0600

    Fix an error in ManejoUsuarios

--- src/DAO/Connection.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-import mysqlRouter from './mysql';
-const app = express();
-const bodyParser = require('body-parser');
-const { query, request} = require('express');
-
-app.set('views', '../Vista');
-app.use('/usuario', mysqlRouter);
