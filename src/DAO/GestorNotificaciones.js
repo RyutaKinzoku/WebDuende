dddb34b8d4be03917da01d2f8229749cd5c3f454
Kenneth Ibarra Vargas <kiv19022000@gmail.com>
2021-11-25 16:35:09 -0600

    Update GestorNotificaciones.js

--- src/DAO/GestorNotificaciones.js
+++ src/DAO/GestorNotificaciones.js
@@ -8,7 +8,7 @@ export default class GestorNotificaciones extends GestorDB{
             id: notificacionCompra.id,
             idOrdenCompra: notificacionCompra.idOrdenCompra
         }
-        return axios.post('http://localhost:3001/api/modificarNotificacionCompra',values);
+        return axios.post('https://web-duende-server.herokuapp.com/api/modificarNotificacionCompra',values);
     }
     async modificarNotificacionCita(notificacionCita){
         let values = {
@@ -17,16 +17,16 @@ export default class GestorNotificaciones extends GestorDB{
             idPublicacion: notificacionCita.idPublicacion,
             correoUsuario: notificacionCita.correoUsuario,
         }
-        return axios.post('http://localhost:3001/api/modificarNotificacionCita',values);
+        return axios.post('https://web-duende-server.herokuapp.com/api/modificarNotificacionCita',values);
     }
     async eliminar(type, idNotificacion){
         let values = {
             idNotificacion: idNotificacion
         }
         if(type === "NotificacionCita"){
-            return axios.post('http://localhost:3001/api/eliminarNotificacionCita',values);
+            return axios.post('https://web-duende-server.herokuapp.com/api/eliminarNotificacionCita',values);
         } else if(type === "NotificacionCompra") {
-            return axios.post('http://localhost:3001/api/eliminarNotificacionCompra',values);
+            return axios.post('https://web-duende-server.herokuapp.com/api/eliminarNotificacionCompra',values);
         }
     }
 
@@ -35,19 +35,19 @@ export default class GestorNotificaciones extends GestorDB{
             idOrdenCompra: idOrdenCompra
         }
         if(type === "NotificacionCompra") {
-            return axios.post('http://localhost:3001/api/eliminarNotificacionDesdeOrden',values);
+            return axios.post('https://web-duende-server.herokuapp.com/api/eliminarNotificacionDesdeOrden',values);
         }
     }
     async obtener(type, idNotificacion){
         if(type === "NotificacionCita"){
-            var response = await axios.get('http://localhost:3001/api/obtenerNotificacionCita',{params: {idNotificacion: idNotificacion} });
+            var response = await axios.get('https://web-duende-server.herokuapp.com/api/obtenerNotificacionCita',{params: {idNotificacion: idNotificacion} });
             if(response.data.length>0){
                 let notificacionCita = response.data[0];
                 let c = FabricaNotificaciones.fabricarNotificacion("NotificacionCita;"+notificacionCita.idNotificacion+";"+notificacionCita.idPublicacion+";"+notificacionCita.correo+";"+notificacionCita.mensaje+";"+notificacionCita.lugar +";"+notificacionCita.vista);
                 return c;
             }
         }else if(type === "NotificacionCompra"){
-            var response = await axios.get('http://localhost:3001/api/obtenerNotificacionCompra',{params: {idNotificacion: idNotificacion} });
+            var response = await axios.get('https://web-duende-server.herokuapp.com/api/obtenerNotificacionCompra',{params: {idNotificacion: idNotificacion} });
             if(response.data.length>0){
                 let notificacionCompra = response.data[0];
                 let c = FabricaNotificaciones.fabricarNotificacion("NotificacionCompra;"+notificacionCompra.idNotificacion+";"+notificacionCompra.idOrden+";"+notificacionCompra.vista);
@@ -60,7 +60,7 @@ export default class GestorNotificaciones extends GestorDB{
             id: notificacionCompra.id,
             idOrdenCompra: notificacionCompra.idOrdenCompra
         }
-        return axios.post('http://localhost:3001/api/agregarNotificacionCompra',values);
+        return axios.post('https://web-duende-server.herokuapp.com/api/agregarNotificacionCompra',values);
     }
     async agregarNotificacionCita(notificacionCita){
         let values = {
@@ -71,11 +71,11 @@ export default class GestorNotificaciones extends GestorDB{
             lugar: notificacionCita.lugar
         }
 
-        return axios.post('http://localhost:3001/api/agregarNotificacionCita',values);
+        return axios.post('https://web-duende-server.herokuapp.com/api/agregarNotificacionCita',values);
     }
     async obtenerLista(){
-        let notificacionesCita = await axios.get('http://localhost:3001/api/getNotificacionesCita');
-        let notificacionesCompra = await axios.get('http://localhost:3001/api/getNotificacionesCompra');
+        let notificacionesCita = await axios.get('https://web-duende-server.herokuapp.com/api/getNotificacionesCita');
+        let notificacionesCompra = await axios.get('https://web-duende-server.herokuapp.com/api/getNotificacionesCompra');
         let notificaciones = [];
         notificacionesCita.data.forEach(element => {
             let notificacionCita = FabricaNotificaciones.fabricarNotificacion("NotificacionCita;"+element.ID+";"+element.idPublicacion+";"+element.correoUsuario+";"+element.mensaje+";"+element.lugar+";"+element.vista)
@@ -88,12 +88,12 @@ export default class GestorNotificaciones extends GestorDB{
         return notificaciones;
     }
     async getNext(){
-        let valor = await axios.get('http://localhost:3001/api/getNextNotificaciones');
+        let valor = await axios.get('https://web-duende-server.herokuapp.com/api/getNextNotificaciones');
         return valor.data[0].ultimo_valor;
     }
 
     async obtenerNotificacionesCita(){
-        let notificacionesCita = await axios.get('http://localhost:3001/api/getNotificacionesCita');
+        let notificacionesCita = await axios.get('https://web-duende-server.herokuapp.com/api/getNotificacionesCita');
         let notificaciones = [];
         notificacionesCita.data.forEach(element => {
             let notificacionCita = FabricaNotificaciones.fabricarNotificacion("NotificacionCita;"+element.ID+";"+element.idPublicacion+";"+element.correoUsuario+";"+element.mensaje+";"+element.lugar+";"+element.vista)
@@ -103,7 +103,7 @@ export default class GestorNotificaciones extends GestorDB{
     }
 
     async obtenerNotificacionesCompra(){
-        let notificacionesCompra = await axios.get('http://localhost:3001/api/getNotificacionesCompra');
+        let notificacionesCompra = await axios.get('https://web-duende-server.herokuapp.com/api/getNotificacionesCompra');
         let notificaciones = [];
         notificacionesCompra.data.forEach(element => {
             let notificacionCompra = FabricaNotificaciones.fabricarNotificacion("NotificacionCompra;"+element.ID+";"+element.idOrdenCompra+";"+element.vista);
