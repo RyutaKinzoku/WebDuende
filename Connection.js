280ee0dd9c58584e9e2160ee7fa9184fd3ed72ed
Kenneth Ibarra Vargas <kiv19022000@gmail.com>
2021-11-28 15:21:20 -0600

    Update Connection.js

--- Connection.js
+++ Connection.js
@@ -12,6 +12,6 @@ app.use('/api', require('./mysql.js'));
 app.use('/api', require('./mongo.js'));
 app.use('/api', require('./redis.js'));
 app.use(express.static('client/build'));
-app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html'));
+app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html')));
 
 app.listen(PORT, console.log("running on port "+ PORT));
