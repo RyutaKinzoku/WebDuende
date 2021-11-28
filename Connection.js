819e2e9f356d9f75dd6d6bda1590a4f8eadf3140
Kenneth Ibarra Vargas <kiv19022000@gmail.com>
2021-11-28 15:20:29 -0600

    Fix routing

--- Connection.js
+++ Connection.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use('/api', require('./mysql.js'));
 app.use('/api', require('./mongo.js'));
 app.use('/api', require('./redis.js'));
-
 app.use(express.static('client/build'));
+app.get('*', (req, res) => res.sendFile(path.resolve('client/build', 'index.html'));
 
 app.listen(PORT, console.log("running on port "+ PORT));
